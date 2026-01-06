f292d1dfe7924047f190dc8bfac07d71f7d5b2df
safal <E01046@ycosolutions.com>
2026-01-06 09:59:00 +0545

    impl two sum solution

---
+++ src/main/java/com/learn/leetcode/TwoSum.java
@@ -0,0 +1,39 @@
+package com.learn.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public static int [] twoSumSolution(int[] nums, int target){
+        //* method 1: sorted array
+//        int start = 0;
+//        int end = nums.length-1;
+//
+//        while (start<end){
+//            if(nums[start]+nums[end] ==target){
+//                return new int [] {start,end};
+//            }else if (nums[start]+nums[end]<target){
+//                start++;
+//            }else {
+//                end--;
+//            }
+//        }
+//
+//        return new int[] {-1,-1};
+
+        int length = nums.length;
+        Map<Integer,Integer> visitedMap = new HashMap<>();
+
+        for(int i =0;i<length;i++){
+            int result = target-nums[i];
+            if(visitedMap.containsKey(result)){
+                return  new int[] {visitedMap.get(result),i};
+            }
+            visitedMap.put(nums[i],i);
+        }
+        return  new int [] {-1,-1};
+    }
+
+
+}
