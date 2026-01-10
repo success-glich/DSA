cb801919a222c53b56973ddeeaee720f6d25c0f6
Saphal Ghorasaini <iamsaphal21@gmail.com>
2026-01-10 11:27:46 +0545

    Impl Best time to sell stock I

---
+++ src/main/java/com/learn/leetcode/BestTimeToSellStock.java
@@ -0,0 +1,19 @@
+package com.learn.leetcode;
+
+public class BestTimeToSellStock {
+
+    public static int bestTimeToSellStockI(int [] prices){
+
+        int min = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i=0;i<prices.length;i++){
+            if(prices[i]<min){
+                min = prices[i];
+            }else if(prices[i]-min>maxProfit){
+                maxProfit = prices[i]-min;
+            }
+        }
+        return maxProfit;
+    }
+}
